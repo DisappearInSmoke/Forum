8e8d28619a8177dc682b94345511d28eb2e804e5
youhoajie <3485594790@qq.com>
2021-01-05 15:17:47 +0800

    YHJ第一次测试修改

--- demo/src/main/java/com/example/demo/HelloServlet.java
+++ demo/src/main/java/com/example/demo/HelloServlet.java
@@ -14,7 +14,7 @@ public class HelloServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
-
+        //这是尤灏洁的项目
         // Hello
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
